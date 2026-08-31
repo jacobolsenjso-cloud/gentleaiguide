3fed105806cf4be51618c5eb647d4464e3fc529c
Jacob Olsen <jacobolsenjso@gmail.com>
2026-08-31 20:59:49 +0200

    Block search engines until content is approved

---
+++ src/site.ts
@@ -0,0 +1,4 @@
+// Én kontakt at slå på, når indholdet er godkendt.
+// INDEXABLE = false  -> sitet er live og læsbart, men søgemaskiner holdes ude
+// INDEXABLE = true   -> sitet må indekseres af Google m.fl.
+export const INDEXABLE = false;
